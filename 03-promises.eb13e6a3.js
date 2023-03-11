2fb586cf82e19aee904cdab68a4732109be70b6d
Tymofii-Shtyrba <113638167+Tymofii-Shtyrba@users.noreply.github.com>
2023-03-11 16:37:18 +0000

    Deploying to gh-pages from @ Tymofii-Shtyrba/goit-js-hw-09@8fe7839d6420e401a2b6ef40ed0a2399fb366974 🚀

---
+++ 03-promises.eb13e6a3.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll("input");document.querySelector("button[type=submit]").addEventListener("click",(t=>{t.preventDefault();const n=function(e){const t={};for(const n of e)t[n.name]=n.value;return t}(e);for(let e=0;e<n.amount;e++)console.log("yes")}));
+//# sourceMappingURL=03-promises.eb13e6a3.js.map
